fb7f4190dad9a8ba4f2edd59d7537f210838c076
Marlos11 <marlos@hotmail.com>
2023-12-28 13:29:20 -0300

    foi criado a função de assitri ao trailer quando climaos no botão.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,11 @@
 import { BtnWatchMovies, BtnWatchTrailer } from "./styles"
 
 // eslint-disable-next-line react/prop-types
-function Button({ children, red }) {
+function Button({ children, red, ...rest }) {
     return (
         <>
-            {red ? (<BtnWatchMovies>{children}</BtnWatchMovies>) : (<BtnWatchTrailer>{children}</BtnWatchTrailer>)}
+            {red ? (<BtnWatchMovies>{children}</BtnWatchMovies>) : 
+            (<BtnWatchTrailer {...rest}>{children}</BtnWatchTrailer>)}
         </>
 
     )
